4b156f176199b8ceaf45f504c57a028cc49abe90
Jonas Woerlein <jonas@woerlein.net>
2015-07-05 19:53:53 +0200

    Further openshift adaptions

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var express = require('express')
 var app = express();
 
 app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 5000))
+app.set('ip', (process.env.OPENSHIFT_NODEJS_IP))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello TNG! I\'m running at' + process.env.OPENSHIFT_APP_DNS)
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Node app is running at" +  app.get('ip') + ":" + app.get('port'))
 })
