094f0ba776968ef9cdb56af9025ac15aa28fe35e
Jonas Woerlein <jonas@woerlein.net>
2015-07-05 19:46:17 +0200

    Adapted to OpenShift

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello TNG! I\'m running at' + process.env.OPENSHIFT_APP_DNS)
 })
 
 app.listen(app.get('port'), function() {
